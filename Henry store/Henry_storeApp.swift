b5c27e9479bf7b4d047b3a3a32caa294ab594c41
CryptexHL <94894610+CryptexHL@users.noreply.github.com>
2023-04-02 23:43:21 +0200

    Initial Commit

---
+++ Henry store/Henry_storeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Henry_storeApp.swift
+//  Henry store
+//
+//  Created by Henry Lai on 02/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Henry_storeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
